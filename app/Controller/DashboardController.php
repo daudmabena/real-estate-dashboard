8dac423022b94f3b89e670d9d438842480bfb640
Anubavam <anubavam@anubavam-desktop.(none)>
2012-07-16 17:12:48 +0530

    Index method changed to DashboradImport Controller to Dashboard Controller

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -16,7 +16,77 @@ class DashboardController extends AppController {
     * @access public
     * @var string
     */
-   //var $uses = array('Dashboard');
+   var $uses = array('Dashboard','Calculation');
+   
+   // {{{ method
+   /**
+    * Controller method Index()
+    *
+    */
+   public function index(){
+     
+     $parameters = array();
+     
+     //Current Year Date
+     $currentYear = date('Y-m-d');
+     
+     // $date = date('Y-m-d',strtotime('2010-01-01 -1 year'));
+     $lastyear = strtotime("-1 year", strtotime($currentYear));
+     
+     // format and display the computed date
+     $lastYear = date("Y-m-d", $lastyear);
+     
+     // ZipCode of city
+     $zipCode = '12207';
+     $city = 'Albany';
+     $lastYearTotalSum = "";
+     $lastYearTotalDivider = "";
+     
+     $previouslastyear = strtotime("-1 year", strtotime($lastYear));
+     
+     $previousLastYear = date("Y-m-d", $previouslastyear);
+     
+     $lastYears = array($lastYear, $previousLastYear);
+     
+     $countYears = count($lastYears);
+     
+     //Array Value for Parameters of tableNAme, fieldName, zipCode, fromDate and toDate
+          
+     $parameters['fieldName'] = 'zip_code';
+     $parameters['selectFieldName'] = 'for_sale_median';
+     $parameters['tableName'] = 'tab_median_price_2years';
+     
+     if($parameters['fieldName'] == "zip_code"){
+          $parameters['fieldValue'] = $zipCode;
+     }
+     else if($parameters['fieldName'] == "city"){
+          $parameters['fieldValue'] = $city;
+     }
+     
+     for($i=0; $i<$countYears; $i++){
+          if($i == 0){
+               $parameters['fromDate'] = $lastYears[0];
+               $parameters['toDate'] = $currentYear;
+          }
+          else{
+               $parameters['fromDate'] = $lastYears[1];
+               $parameters['toDate'] = $lastYears[0];
+          }
+          $this->Calculation->setData($parameters);
+          $lastYearValue = $this->Calculation->calculateMedian12months();
+          if($i == 1){
+               //Getting Divider here
+               $lastYearTotalDivider = $lastYearValue;
+          }
+          //Getting Sum of total year with last Two Years
+          echo $lastYearValue."</br>";
+          $lastYearTotalSum += $lastYearValue;
+          //$countYears--;
+     }
+     echo $lastYearTotalSum/$lastYearTotalDivider;
+     $this->autoRender = false;
+   }
+   
    
    // {{{ method
    /**
--- app/Controller/DashboardImportsController.php
+++ app/Controller/DashboardImportsController.php
@@ -30,78 +30,6 @@ class DashboardImportsController extends AppController {
         $this->render('dashboardImport');
    }
    
-   
-   // {{{ method
-   /**
-    * Controller method Index()
-    *
-    */
-   public function index(){
-     
-    $parameters = array();
-     
-     $this->set('countries', $this->AppModel->bind_country());
-     
-     //Current Year Date
-     $currentYear = date('Y-m-d');
-     
-     // $date = date('Y-m-d',strtotime('2010-01-01 -1 year'));
-     $lastyear = strtotime("-1 year", strtotime($currentYear));
-     
-     // format and display the computed date
-     $lastYear = date("Y-m-d", $lastyear);
-     
-     // ZipCode of city
-     $zipCode = '12207';
-     $city = 'Albany';
-     $lastYearTotalSum = "";
-     $lastYearTotalDivider = "";
-     
-     $previouslastyear = strtotime("-1 year", strtotime($lastYear));
-     
-     $previousLastYear = date("Y-m-d", $previouslastyear);
-     
-     $lastYears = array($lastYear, $previousLastYear);
-     
-     $countYears = count($lastYears);
-     
-     //Array Value for Parameters of tableNAme, fieldName, zipCode, fromDate and toDate
-          
-     $parameters['fieldName'] = 'zip_code';
-     $parameters['selectFieldName'] = 'for_sale_median';
-     $parameters['tableName'] = 'tab_median_price_2years';
-     
-     if($parameters['fieldName'] == "zip_code"){
-          $parameters['fieldValue'] = $zipCode;
-     }
-     else if($parameters['fieldName'] == "city"){
-          $parameters['fieldValue'] = $city;
-     }
-     
-     for($i=0; $i<$countYears; $i++){
-          if($i == 0){
-               $parameters['fromDate'] = $lastYears[0];
-               $parameters['toDate'] = $currentYear;
-          }
-          else{
-               $parameters['fromDate'] = $lastYears[1];
-               $parameters['toDate'] = $lastYears[0];
-          }
-          $this->Calculation->setData($parameters);
-          $lastYearValue = $this->Calculation->calculateMedian12months();
-          if($i == 1){
-               //Getting Divider here
-               $lastYearTotalDivider = $lastYearValue;
-          }
-          //Getting Sum of total year with last Two Years
-          echo $lastYearValue."</br>";
-          $lastYearTotalSum += $lastYearValue;
-          //$countYears--;
-     }
-     echo $lastYearTotalSum/$lastYearTotalDivider;
-   }
-   
-   
    // {{{ method
    /**
     * Controller method dataParser()
@@ -170,48 +98,18 @@ class DashboardImportsController extends AppController {
      exit;
    }
    
+   
    public function getState(){
-          Controller::loadModel('Dashboard');
-          $q = strtoupper($_REQUEST["q"]);
-          if (!$q) return;
-          
-          $stateCode = $this->Dashboard->getStateCode($q);
-          
-          for($i=0;$i<count($stateCode);$i++){
-               echo $stateCode[$i]."\n";
-          }
-          exit;
-   }
-   
-   public function getCity(){
-          $selectedStateCode = $_REQUEST['stateVal'];
-          Controller::loadModel('Dashboard');
-          
-          $q = strtoupper($_REQUEST["q"]);
-          if (!$q) return;
-          
-          $cityCode = $this->Dashboard->getCityCode($q, $selectedStateCode);
+     Controller::loadModel('Dashboard');
+     $q = strtoupper($_REQUEST["q"]);
+     if (!$q) return;
      
-          for($i=0;$i<count($cityCode);$i++){
-               echo $cityCode[$i]."\n";
-          }
-          exit;
-   }
-   
-   public function getZipCode(){
-          $selectedCity = $_REQUEST['cityVal'];
-          Controller::loadModel('Dashboard');
-          
-          $q = (string)$_REQUEST["q"];
-          
-          if (!isset($q)) return;
-         
-          $zipCode = $this->Dashboard->getZipCodes($q, $selectedCity);
+     $stateCode = $this->Dashboard->getStateCode($q);
      
-          for($i=0;$i<count($zipCode);$i++){
-               echo $zipCode[$i]."\n";
-          }
-          exit;
+     for($i=0;$i<count($stateCode);$i++){
+          echo $stateCode[$i]."\n";
+     }
+     exit;
    }
 }
 ?>
