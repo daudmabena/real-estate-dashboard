7202d8ca8e577ab57b27cee8c322620ad6008b1f
gopi <gopinath@anubavam.com>
2012-07-20 18:22:29 +0530

    dashboard update for debuging

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -220,7 +220,7 @@ class DashboardController extends AppController {
      $this->Calculation->setData($args);
      $finalInputToJson['groupByMonthAndYearForMedian'] = $this->Calculation->groupBymonthWiseWithDifferentYears();
 
-     echo json_encode($finalInputToJson);
+     echo "Thisd is Test".json_encode($finalInputToJson);
      $this->autoRender = false;
    }
    
