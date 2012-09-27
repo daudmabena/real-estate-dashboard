53066cf9c9452f90cac0fe1b652641f26538d601
gopinath <gopinath@anubavam.com>
2012-09-27 17:21:59 +0530

    sam

---
+++ app/webroot/sample.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+<body>
+
+<iframe src="http://dev.realestatedashboard.anubavam.info" width="100%" height="1000px">
+  <p>Your browser does not support iframes.</p>
+</iframe>
+
+</body>
+</html>
