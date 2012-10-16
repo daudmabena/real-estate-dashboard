2437671b54a3a7f5dbaa9d46ede6dcf46667ceac
gopinath <gopinath@anubavam.com>
2012-10-16 16:26:23 +0530

    dashboard

--- app/webroot/js/dashboard.js
+++ app/webroot/js/dashboard.js
@@ -381,7 +381,7 @@ function generateChart(monthlyTotal,year){
     widthChart = 340;
   }
   else if(screenWidth == 768){
-    widthChart = 393;
+    widthChart = 395;
   }
     $(function () {
         var chart;
